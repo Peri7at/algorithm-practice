2007c9d8f439c3d7fef8f2111a6c3f5f451daa0f
Perizat Ibraeva <perizat.ibraeva.k@gmail.com>
2022-10-17 23:31:49 +0300

    add optimal solution

--- count-meeting-rooms.js
+++ count-meeting-rooms.js
@@ -1,7 +1,22 @@
 // Count how many meeting rooms will be needed according to given array of starting and ending hours of meetings.
 // F.ex [[10,12],[11,14],[15,17],[17,18]] this schedule needs 2 rooms.
 
+// Following solution has constant space complexity and linear time complexity
 const countRooms = (arr) => {
+  const rooms = 0;
+  const meetings = Array(24).fill(0);
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = arr[i][0]; j < arr[i][1]; j++) {
+      meetings[j]++;
+      meetings[j] > max ? (max = meetings[j]) : max;
+    }
+  }
+  return max;
+};
+
+// Alternative solution (not optimal):
+const countRoomsAlt = (arr) => {
   const rooms = 0;
   const schedules = [];
   const meetings = {};
